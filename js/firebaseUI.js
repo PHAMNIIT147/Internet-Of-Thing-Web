7a02702969b30e27793d0b5571ba31138db59ee2
PHONG PHAM <37699545+PHAMNIIT147@users.noreply.github.com>
2020-03-15 12:13:17 +0700

    Update firebaseUI.js

--- js/firebaseUI.js
+++ js/firebaseUI.js
@@ -18,14 +18,14 @@ $(function() {
         },
         // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
         signInFlow: 'popup',
-        signInSuccessUrl: 'index.html',
+        signInSuccessUrl: 'm.html',
         signInOptions: [
             // Leave the lines as is for the providers you want to offer your users.
             firebase.auth.EmailAuthProvider.PROVIDER_ID,
             firebase.auth.GoogleAuthProvider.PROVIDER_ID,
         ],
         // Terms of service url.
-        tosUrl: 'index.html',
+        tosUrl: 'main.html',
         // Privacy policy url.
         privacyPolicyUrl: 'https://firebase.google.com/docs/auth/web/firebaseui'
     };
