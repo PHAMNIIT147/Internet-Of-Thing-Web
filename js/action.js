b55326061f67533702e080ef3001645c5c5e9694
PHAMNIIT147 <phongpham663@gmail.com>
2020-04-18 17:26:47 +0700

    :package: update fontEnd for nCovid Global get api from trackvirus.com

---
+++ js/action.js
@@ -0,0 +1,5 @@
+anotherTable = "<table id = 'table' class = 'table-dark' data-toolbar = '#toolbar' data-sort-name = 'totalcase' data-sort-order = 'desc' data-show-footer = 'true' data-pagination = 'false' data-search = 'true' data-page-list = '[10 ,20, 50,100,all]' data-show-toggle = 'true' data-show-columns = 'true' data-show-columns-show-toggle-all = 'true' data-show-export ='true' data-show-pagination-switch = 'true'> </table>";
+
+function myFunction() {
+    document.getElementById('anotherView').innerHTML = anotherTable;
+}
---
+++ js/api/ncovid_another.js
@@ -0,0 +1,68 @@
+axios.get('https://api.thevirustracker.com/free-api?countryTotals=ALL').then(function(response) {
+    let rawData = arrData(response.data.countryitems[0]);
+    myTable(rawData);
+}).catch(function(error) {
+    console.log(error);
+})
+
+function arrData(objData) {
+    let arrayData = Object.keys(objData).map(function(key) {
+        return objData[key]
+    });
+    return myData(arrayData);
+}
+
+function myData(data) {
+    let list = [];
+    let getRecovered = [];
+    let getDeath = [];
+    let getConfirm = [];
+
+    let sumRecovered = 0;
+    let sumDeath = 0;
+    let sumConfirm = 0;
+    data.forEach(function(item) {
+        list.push([
+            item.title,
+            item.total_cases,
+            item.total_deaths,
+            item.total_recovered,
+            item.total_active_cases,
+        ]);
+        getRecovered.push(item.total_recovered);
+        getDeath.push(item.total_deaths);
+        getConfirm.push(item.total_cases);
+    });
+    console.log(list.slice(0, 182));
+    for (let i = 0; i < getRecovered.length; i++) {
+        /* preccing NaN */
+        if (isNaN(getRecovered[i])) {
+            break;
+        }
+        sumDeath += getDeath[i];
+        sumRecovered += getRecovered[i];
+        sumConfirm += getConfirm[i];
+    }
+    console.log("Recovered: " + sumRecovered, "Deaths: " + sumDeath, "Confirmed: " + sumConfirm);
+    document.getElementById('deaths').innerText = sumDeath;
+    document.getElementById('recovereds').innerText = sumRecovered;
+    document.getElementById('confirmeds').innerText = sumConfirm;
+    return list.slice(0, 182);
+}
+
+function myTable(dataSet) {
+    let myDataTable = $('#datatable').DataTable({
+        data: dataSet,
+        columns: [
+            { title: "Countries and territories" },
+            { title: "Cases" },
+            { title: "Deaths" },
+            { title: "Recoveries" },
+            { title: "Active" }
+        ],
+        order: [
+            [1, "desc"]
+        ]
+    });
+    return myDataTable;
+}
