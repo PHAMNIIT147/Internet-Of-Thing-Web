594fa5a3cf869ba42d07d477e32ffb493f8faf93
PHAMNIIT147 <phongpham663@gmail.com>
2020-04-27 22:50:39 +0700

    :art: navigation page

--- assets/js/include_file.js
+++ assets/js/include_file.js
@@ -1,6 +1,14 @@
+const mergeSort = document.getElementById("mergeSort");
+
 $(function() {
     $('#navbar').load('include/header.html');
     $('#footer').load('include/footer.html');
     $('#home').load('page/views/home.html');
-    $('#mergeSort').load('page/post/mergeSort.html');
+
+    mergeSort.addEventListener('click', event => {
+        $('#viewMS').load('page/post/mergeSort.html');
+        $('#comment').load('page/comment.html');
+    });
+
+
 });
